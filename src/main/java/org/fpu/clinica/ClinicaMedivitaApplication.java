d1e2169c7e05ced938d21498c7b5eb2b9284b9e3
Augusto Branquinho <augustobranquinho@gmail.com>
2017-09-08 19:49:52 -0300

    Ignorando arquivos de IDEs.

--- src/main/java/org/fpu/clinica/ClinicaMedivitaApplication.java
+++ src/main/java/org/fpu/clinica/ClinicaMedivitaApplication.java
@@ -1,9 +1,5 @@
 package org.fpu.clinica;
 
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-import java.util.Locale;
-
 import org.apache.log4j.LogManager;
 import org.apache.log4j.Logger;
 import org.springframework.boot.SpringApplication;
@@ -13,25 +9,26 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.FixedLocaleResolver;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Locale;
+
 @SpringBootApplication
 public class ClinicaMedivitaApplication {
 	private static final Logger LOGGER = LogManager.getLogger(ClinicaMedivitaApplication.class);
 
 	public static void main(String[] args) throws UnknownHostException {
-		
-		
 		ApplicationContext app = SpringApplication.run(ClinicaMedivitaApplication.class, args);
 
 		String applicationName = app.getEnvironment().getProperty("spring.application.name");
 		String contextPath = app.getEnvironment().getProperty("server.contextPath");
 		String port = app.getEnvironment().getProperty("server.port");
 		String hostAddress = InetAddress.getLocalHost().getHostAddress();
-		 LOGGER.info("\n|------------------------------------------------------------" +
-	                "\n| Application '" + applicationName + "' is running! Access URLs:" +
-	                "\n|   Local:      http://127.0.0.1:" + port + contextPath +
-	                "\n|   External:   http://" + hostAddress + ":" + port + contextPath +
-	"\n|------------------------------------------------------------");
-	
+	    LOGGER.info("\n|------------------------------------------------------------" +
+				"\n| Application '" + applicationName + "' is running! Access URLs:" +
+				"\n|   Local:      http://127.0.0.1:" + port + contextPath +
+				"\n|   External:   http://" + hostAddress + ":" + port + contextPath +
+				"\n|------------------------------------------------------------");
 	}
 
 	@Bean
