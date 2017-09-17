239acaa3430a3de8e74426af2b81e025e480866b
LeonardoRSilva <leonardo.rsil@gmail.com>
2017-09-16 22:51:58 -0300

    add steps in newEscalaDialog

--- src/main/webapp/appsrc/escala/dialog/newEscalaDialogController.js
+++ src/main/webapp/appsrc/escala/dialog/newEscalaDialogController.js
@@ -3,10 +3,60 @@
 
 angular.module('clinica')
 
-    .controller('NewEscalaDialogController', function ($mdDialog, $scope, $mdMedia, $mdToast, RestSrv, SERVICE_PATH, $rootScope, $mdUtil) {
+    .controller('NewEscalaDialogController', function ($mdDialog, $scope, $mdMedia, $mdToast, $q, $timeout, RestSrv, SERVICE_PATH, $rootScope, $mdUtil) {
 
 
 
+
+
+
+        $scope.selectedStep = 0;
+        $scope.stepProgress = 1;
+        $scope.maxStep = 3;
+        $scope.showBusyText = false;
+        $scope.stepData = [
+            { step: 1, completed: false, optional: false, data: {} },
+            { step: 2, completed: false, optional: false, data: {} },
+            { step: 3, completed: false, optional: false, data: {} },
+        ];
+
+         $scope.enableNextStep = function nextStep() {
+                //do not exceed into max step
+                if ($scope.selectedStep >= $scope.maxStep) {
+                    return;
+                }
+                //do not increment $scope.stepProgress when submitting from previously completed step
+                if ($scope.selectedStep === $scope.stepProgress - 1) {
+                    $scope.stepProgress = $scope.stepProgress + 1;
+                }
+                $scope.selectedStep = $scope.selectedStep + 1;
+            }
+
+            $scope.moveToPreviousStep = function moveToPreviousStep() {
+                    if ($scope.selectedStep > 0) {
+                        $scope.selectedStep = $scope.selectedStep - 1;
+                    }
+                }
+
+        $scope.submitCurrentStep = function submitCurrentStep(stepData, isSkip) {
+                var deferred = $q.defer();
+                $scope.showBusyText = true;
+                console.log('On before submit');
+                if (!stepData.completed && !isSkip) {
+                    //simulate $http
+                    $timeout(function () {
+                        $scope.showBusyText = false;
+                        console.log('On submit success');
+                        deferred.resolve({ status: 200, statusText: 'success', data: {} });
+                        //move to next step when success
+                        stepData.completed = true;
+                        $scope.enableNextStep();
+                    }, 1000)
+                } else {
+                    $scope.showBusyText = false;
+                    $scope.enableNextStep();
+                }
+            }
 
 
 
